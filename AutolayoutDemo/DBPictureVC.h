11419f482446d2c3534cd5faf250eb7ce0686886
q858333 <284825870@qq.com>
2015-01-04 14:19:28 +0800

    创建图片VC

---
+++ AutolayoutDemo/DBPictureVC.h
@@ -0,0 +1,13 @@
+//
+//  DBPictureVC.h
+//  AutolayoutDemo
+//
+//  Created by dengbin on 15/1/4.
+//  Copyright (c) 2015年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBPictureVC : UIViewController
+
+@end
