865c6b78f4736c67589646693fe1766adaa0a9c2
孙 化育 <sunkasoda@sunmatoMacBook-Pro.local>
2015-01-06 14:37:06 +0800

    添加了评论界面

---
+++ AutolayoutDemo/View and ViewControllers/CommentVC.h
@@ -0,0 +1,13 @@
+//
+//  CommentVC.h
+//  AutolayoutDemo
+//
+//  Created by 孙 化育 on 15-1-6.
+//  Copyright (c) 2015年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentVC : UIViewController
+
+@end
---
+++ AutolayoutDemo/View and ViewControllers/CommentVC.m
@@ -0,0 +1,37 @@
+//
+//  CommentVC.m
+//  AutolayoutDemo
+//
+//  Created by 孙 化育 on 15-1-6.
+//  Copyright (c) 2015年 song. All rights reserved.
+//
+
+#import "CommentVC.h"
+
+@interface CommentVC ()
+
+@end
+
+@implementation CommentVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
